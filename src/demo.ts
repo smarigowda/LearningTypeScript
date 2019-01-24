ddab17b21f0a5484b370a3ff8701c42f251afda7
Santosh Marigowda <santosharakere@gmail.com>
2019-01-24 20:58:09 +0000

    initial commit

---
+++ src/demo.ts
@@ -0,0 +1,16 @@
+function greet(name: string) {
+    console.log('hello...');
+}
+
+greet('Santosh Marigowda');
+
+let myName: string = 'Santosh Marigowda';
+// myName = 20;
+
+let myAge = 45;
+// myAge = 'Santosh';
+
+let hasHobbies: boolean = true;
+// hasHobbies = 45;
+
+let myRealAge: number = 46;
